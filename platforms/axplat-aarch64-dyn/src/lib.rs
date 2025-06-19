7325579b3f67e2778b591a2b89b08c480ded747e
周睿 <zrufo747@outlook.com>
2025-06-19 17:42:01 +0800

    update

--- platforms/axplat-aarch64-dyn/src/lib.rs
+++ platforms/axplat-aarch64-dyn/src/lib.rs
@@ -18,20 +18,10 @@ mod config {
     axconfig_macros::include_configs!(path_env = "AX_CONFIG_PATH", fallback = "axconfig.toml");
 }
 
-// Use `.data` section to prevent being cleaned by `clean_bss`.
-#[unsafe(link_section = ".data")]
-static BOOT_INFO: Once<BootInfo> = Once::new();
-
 #[pie_boot::entry]
 fn main(args: &BootInfo) -> ! {
-    BOOT_INFO.call_once(move || args.clone());
-
     axplat::call_main(
         args.cpu_id,
         args.fdt.map(|p| p.as_ptr() as usize).unwrap_or_default(),
     );
-}
-
-fn boot_info() -> &'static BootInfo {
-    BOOT_INFO.wait()
 }
