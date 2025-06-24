a03e1bdf7653418a5daa947889e2230f6fbaab03
周睿 <zrufo747@outlook.com>
2025-06-24 17:08:30 +0800

    更新 axplat-aarch64-dyn 平台的依赖项，移除不必要的版本号，添加物理地址与虚拟地址转换函数，完善时间管理功能。

--- platforms/axplat-aarch64-dyn/src/init.rs
+++ platforms/axplat-aarch64-dyn/src/init.rs
@@ -68,13 +68,13 @@ impl InitIf for InitIfImpl {
     /// * Timer interrupts are enabled (if applicable).
     /// * Other platform devices are initialized.
     fn init_later(cpu_id: usize, arg: usize) {
-        todo!()
+       
     }
 
     /// Initializes the platform at the later stage for secondary cores.
     ///
     /// See [`init_later`] for details.
     fn init_later_secondary(cpu_id: usize) {
-        todo!()
+         
     }
 }
